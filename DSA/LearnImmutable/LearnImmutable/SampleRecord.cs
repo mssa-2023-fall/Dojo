b30f83543001e29c2e0ad4d3b3a0cbfdf2dfb754
Geoff-San <geoffrey.sanford@outlook.com>
2023-09-05 13:08:29 -0500

    New Branch init

--- DSA/LearnImmutable/LearnImmutable/SampleRecord.cs
+++ DSA/LearnImmutable/LearnImmutable/SampleRecord.cs
@@ -4,5 +4,4 @@
     {
         public string? MutableProperty { get; set; }
     }
-    public record class SampleNameRecord(string FirstName, string LastName);
 }
--- DSA/LearnImmutable/LearnImmutableTest/SampleRecordTest.cs
+++ DSA/LearnImmutable/LearnImmutableTest/SampleRecordTest.cs
@@ -3,11 +3,17 @@ namespace LearnImmutableTest
     [TestClass]
     public class SampleRecordTest
     {
+        SampleRecord record1 = null;
+        [TestInitialize]
+        public void TestSetup()
+        {
+            record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5))
+            { MutableProperty = "InitialString" };
+        }
         [TestMethod]
         public void TestRecordTypeEqualityWithPositionParam()
         {
-            //arragne
-            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
+            //arrange
             SampleRecord record2 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
 
             //assert
@@ -29,7 +35,6 @@ namespace LearnImmutableTest
         public void TestRecordTypeSamenessWithPositionParam()
         {
             //arragne
-            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
             SampleRecord record2 = record1;
 
             //assert
@@ -40,7 +45,6 @@ namespace LearnImmutableTest
         public void TestRecordTypeAutoImplementedProperties()
         {
             //Arrange
-            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
 
             //Assert
             Assert.AreEqual("Test", record1.ParamString);
@@ -52,14 +56,30 @@ namespace LearnImmutableTest
         {
             //Arrange
             string expected = "NewString";
-            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5))
-            { MutableProperty = "InitialString" };
-
             //Act
             record1.MutableProperty = expected;
             //Assert
             Assert.AreEqual(record1.MutableProperty, expected);
             
+        }
+        [TestMethod]
+        public void TestRecordTypeNonDestructiveMutation()
+        {
+            //Arrange
+
+            //Act
+            SampleRecord record2 = record1 with { ParamInt = 2 };
+            //Asserts
+            Assert.AreNotEqual(record1, record2); //there are 2 different objects - quality comparison
+            Assert.AreNotSame(record1, record2); //record2 points to different instance than record1 - reference comparison
+            Assert.AreEqual(record2.ParamInt, 2); //record2 has updated ParamInt
+            Assert.AreEqual(record1.ParamInt, 1); //record 1 is immutable
+            Assert.AreEqual(record2.ParamString, "Test"); //record2 has the same properties as record1 if the property ahs not been modified
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+
         }
     }
 }
