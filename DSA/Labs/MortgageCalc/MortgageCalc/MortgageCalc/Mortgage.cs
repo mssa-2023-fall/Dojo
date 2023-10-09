203dc8007e739431303855ce3c466bf24f42a465
Geoff-San <geoffrey.sanford@outlook.com>
2023-10-09 08:21:48 -0500

    unfinished mortgage calc

---
+++ DSA/Labs/MortgageCalc/MortgageCalc/MortgageCalc/Mortgage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageCalc
+{
+    public enum Duration
+    {
+        TenYears = 10,
+        FifteenYears = 15,
+        ThirtyYears = 30
+    }
+    public class Mortgage
+    {
+        //prop
+        public Duration MortgageDuration { get; set; }
+        public double InterestRate { get; set; }
+        public double PrincipalAmount { get; set; }
+        public DateTime OriginationDate { get; set; }
+        public List<Payment> Payments { get; private set; }
+
+        //ctor
+        public Mortgage(Duration duration, double interestRate, double principleAmount, DateTime originationDate)
+        {
+            MortgageDuration = duration;
+            InterestRate = interestRate;
+            PrincipalAmount = principleAmount;
+            OriginationDate = originationDate;
+            Payments = new List<Payment>();
+
+            GenerateAmortizationSchedule();
+        }
+
+        //Methods
+
+        //generate amortization sched. & populate payments list
+        private void GenerateAmortizationSchedule()
+        {
+            //calc monthly interestrate
+            double monthlyInterestRate = (InterestRate / 100) / 12;
+            //calc total payments
+            int totalPayments = (int)MortgageDuration * 12;
+            //calc monthly payment using EMI
+            double monthlyPayment = PrincipalAmount * monthlyInterestRate /
+                                    (1 - Math.Pow(1 + monthlyInterestRate, -totalPayments));
+            DateTime paymentDate = OriginationDate.AddMonths(1);
+            double remainingPrincipal = PrincipalAmount;
+            for(int i = 0; i < totalPayments; i++)
+            {
+                double interestForMonth = remainingPrincipal * monthlyInterestRate;
+                double principalForMonth = monthlyPayment - interestForMonth;
+
+                Payments.Add(new Payment
+                {
+                    Date = paymentDate,
+                    InterestAmount = interestForMonth,
+                    PrincipalAmount = principalForMonth,
+                    Total = monthlyPayment
+                });
+            }
+        }
+        public DateTime GetPayoffDate()
+        {
+            return OriginationDate.AddYears((int)MortgageDuration);
+        }
+        public double RemainingPrincipalAtDate(DateTime date)
+        {
+            //calc remaining principal at the given date
+            return 0;
+        }
+        public double InterestPaidAtDate(DateTime date)
+        {
+            //calc total interest paid up the set date
+            return 0;
+        }
+        public List<Payment> GetAmortizationSchedule()
+        {
+            return Payments;
+        }
+        public class Payment
+        {
+            public DateTime Date { get; set; }
+            public double PrincipalAmount { get; set; }
+            public double InterestAmount { get; set; }
+            public double Total { get; set; }
+        }
+    }
+}
---
+++ DSA/Labs/MortgageCalc/MortgageCalc/MortgageCalc/Program.cs
@@ -0,0 +1,43 @@
+﻿using MortgageCalc;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Mortgage Calculator:\n" +
+                          "Enter the home's principal amount:");
+        int principalAmt = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Will this be a 10, 15, or 30 year loan?");
+        Duration duration;
+        switch(int.Parse(Console.ReadLine()))
+        {
+            case 10:
+                duration = Duration.TenYears;
+                break;
+            case 15:
+                duration = Duration.FifteenYears;
+                break;
+            case 30:
+                duration = Duration.ThirtyYears;
+                break;
+            default:
+                Console.WriteLine("Invalid duration. Defaulting to 15 year loan.");
+                duration = Duration.FifteenYears;
+                break;
+        }
+
+        Console.WriteLine("When will the loan start? Format: mm/dd/yyyy");
+        DateTime originationDate = DateTime.Parse(Console.ReadLine());
+        
+        Console.WriteLine("What is the interest rate? Format: 4.5 = 4.5%");
+        double interestRate = double.Parse(Console.ReadLine());
+
+        //list out the values
+        Console.WriteLine("Inputs:" +
+                          $"Principal Amount: {principalAmt}\n" +
+                          $"Loan Length: {duration}\n" +
+                          $"Loan Start Date: {originationDate}\n" +
+                          $"Interest Rate: {interestRate}");
+    }
+}
